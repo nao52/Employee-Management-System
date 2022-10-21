4025efd063d3e40e825b1aed00112b20cdc703ab
nao52 <naoki.52abc@icloud.com>
2022-10-21 12:02:57 +0900

    所属部署名の表示を追加

--- src/main/java/com/example/form/UserDetailForm.java
+++ src/main/java/com/example/form/UserDetailForm.java
@@ -2,6 +2,8 @@ package com.example.form;
 
 import java.util.Date;
 
+import com.example.domain.Department;
+
 import lombok.Data;
 
 @Data
@@ -13,5 +15,6 @@ public class UserDetailForm {
 	private Date birthday;
 	private Integer age;
 	private Integer gender;
+	private Department department;
 
 }
