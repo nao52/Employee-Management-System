d93a8ecc7594b9d6935d454e2fce7dd78c7ad0b6
nao52 <naoki.52abc@icloud.com>
2022-10-22 12:22:48 +0900

    パスワードの暗号化

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	/** セキュリティの対象外を設定 */
 	@Override
@@ -45,10 +53,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	/** 認証の設定 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		PasswordEncoder encoder = passwordEncoder();
+
 		// インメモリ認証
 		auth.inMemoryAuthentication().withUser("user") // userを追加
-				.password("user").roles("GENERAL").and().withUser("admin") // adminを追加
-				.password("admin").roles("ADMIN");
+				.password(encoder.encode("user")).roles("GENERAL").and().withUser("admin") // adminを追加
+				.password(encoder.encode("admin")).roles("ADMIN");
 	}
 
 }
--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +18,19 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserMapper mapper;
 
+	@Autowired
+	private PasswordEncoder encoder;
+
 	/** ユーザー登録 */
 	@Override
 	public void signup(MUser user) {
 		user.setDepartmentId(1); // 部署
 		user.setRole("ROLE_GENERAL"); // ロール
+
+		// パスワード暗号化
+		String rawPassword = user.getPassword();
+		user.setPassword(encoder.encode(rawPassword));
+
 		mapper.insertOne(user);
 	}
 
@@ -40,7 +49,10 @@ public class UserServiceImpl implements UserService {
 	/** ユーザー更新 (1件) */
 	@Override
 	public void updateUserOne(String userId, String password, String userName) {
-		mapper.updateOne(userId, password, userName);
+		// パスワードの暗号化
+		String encryptPassword = encoder.encode(password);
+
+		mapper.updateOne(userId, encryptPassword, userName);
 	}
 
 	@Override
